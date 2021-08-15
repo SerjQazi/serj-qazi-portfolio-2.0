5c3a05b63d91a8f36fff1326fcb29bf5d4e89a78
Serj Qazi <sirajum.qazi@gmail.com>
2021-08-15 13:05:07 -0400

    initial react setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './styles/styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          SUP?
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
